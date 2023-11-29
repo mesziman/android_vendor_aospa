c0b61c6077a6bfd338514129af22174c216fca64
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-11-29 23:09:34 +0530

    fixes
    Change-Id: I19dcc6b91b3225277db2e6bd07039fcfd49ef220
    
    # Conflicts:
    #	sepolicy/private/platform_app.te
    
    # Conflicts:
    #	overlay/AOSPAFrameworksOverlay/res/values/config.xml

--- products/umi/aospa_umi.mk
+++ products/umi/aospa_umi.mk
@@ -13,7 +13,9 @@
 # limitations under the License.
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+
+
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Include Paranoid Android common configuration
